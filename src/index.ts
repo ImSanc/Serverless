d219b7c43d2a433d1b910c85e0687cc1765cae0c
Sanchit Mishra <sanchit1417@gmail.com>
2024-12-02 22:43:30 +0530

    trying serverless

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,13 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+      return Response.json({
+        message: "you sent a get request"
+      });
+  },
+};
+
